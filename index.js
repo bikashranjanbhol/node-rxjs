eaf0517b1cb6a837e4691ddef9c2d89ac7c45028
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-29 16:18:25 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Rx = require('rxjs');
 const RxOp = require('rxjs/operators');
 
+const validator = require('validator');
+
 const obs = new Rx.Observable((s) => setTimeout(() => s.next(42), 2000));
 const obs2 = new Rx.Observable((s) => setTimeout(() => s.next(43), 1000));
 const obs3 = new Rx.Observable((s) => setTimeout(() => s.next(44), 2000));
@@ -9,4 +11,6 @@ const allObs = Rx.zip([obs, obs2, obs3]);
 
 // obs.subscribe((d) => console.log(d));
 
-allObs.subscribe((d) => console.log('array', d));
+// allObs.subscribe((d) => console.log('array', d));
+
+console.log('Valid Email', validator.isEmail('bikash@gmail.com'));
