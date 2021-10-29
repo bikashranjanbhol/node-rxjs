93376bf7aa35ac969632dbdf18e8b81d237ad76b
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-29 14:10:00 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const obs = new Rx.Observable((s) => setTimeout(() => s.next(42), 2000));
 const obs2 = new Rx.Observable((s) => setTimeout(() => s.next(43), 1000));
 const obs3 = new Rx.Observable((s) => setTimeout(() => s.next(44), 2000));
 
-const allObs = obs.pipe(RxOp.merge(obs2));
+const allObs = RxOp.zip(obs, obs2, obs3);
 
 // obs.subscribe((d) => console.log(d));
 
-allObs.subscribe((d) => console.log('stream', d));
+allObs.subscribe((d) => console.log('zip', d));
