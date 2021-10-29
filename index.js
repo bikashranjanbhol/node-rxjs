3f7eda4f7f2119b32455f21f93aa3a447876a588
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-29 14:04:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const Rx = require('rxjs');
 const RxOp = require('rxjs/operators');
 
 const obs = new Rx.Observable((s) => setTimeout(() => s.next(42), 2000));
+const obs2 = new Rx.Observable((s) => setTimeout(() => s.next(43), 1000));
+const obs3 = new Rx.Observable((s) => setTimeout(() => s.next(44), 2000));
 
-obs.subscribe((d) => console.log(d));
+const allObs = obs.pipe(RxOp.merge(obs2));
+
+// obs.subscribe((d) => console.log(d));
+
+allObs.subscribe((d) => console.log('stream', d));
